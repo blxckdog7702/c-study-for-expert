aab265f9fc0b40c261b3476addd467f1e36648c9
dohun94 <dohun94@naver.com>
2019-01-18 23:18:35 +0900

    create queue.cpp

--- ForExpert/DataStructure/main.cpp
+++ ForExpert/DataStructure/main.cpp
@@ -1,8 +1,8 @@
-#include <stdio.h>
-
-using namespace std;
-
-int main() {
-	printf("Hello World!");
-	return 0;
-}
+//#include <stdio.h>
+//
+//using namespace std;
+//
+//int main() {
+//	printf("Hello World!");
+//	return 0;
+//}
